ae0383258b81bbd09c73142796496e1e6a111342
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-03-03 11:48:21 -0800

    PodcastCard component and styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Jumbotron from "./Components/Jumbotron"
+import PodCastCard from "./Components/PodcastCard"
 
 
 function App() {
   return (
     <Router>
       <NavBar/>
-        <Jumbotron/>
-      <Switch>
+      <Jumbotron/>
+      <PodCastCard/>
       
+      <Switch>
         <Route exact path={["/search", "/"]}  />
         <Route exact path="/mypond"  />
         <Route exact path="*"  />
