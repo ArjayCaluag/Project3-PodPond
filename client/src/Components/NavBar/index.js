4fe763a7def565f5d69c0fea998f6daf6446a009
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-03-03 11:32:16 -0800

    Create base component files

---
+++ client/src/Components/NavBar/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark trimcolor">
+      <Link to="/search" 
+      className="navbar-brand">
+        Pod Pond
+      </Link>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/search" className="nav-link">
+            Search
+            </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/mypond" className="nav-link">
+            My Pond
+            </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
