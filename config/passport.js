c4ef62e9485bffd8fd930175cc7451ffc5295611
Rand Hale <randluca@gmail.com>
2021-03-06 10:32:33 -0800

    preparing for pull to main

--- config/passport.js
+++ config/passport.js
@@ -4,28 +4,28 @@ var LocalStrategy = require("passport-local").Strategy;
 var db = require("../models");
 // import User model, reference as db.User
 
-passport.use(new LocalStrategy(
-    {
-        usernameField: "username"
-    },
-    function (username, password, done) {
-        db.User.findOne({ username }, (err, user) => {
-            if (err) {
-                return done(err);
-            }
-            if (!user) {
-                return done(null, false, { message: "Incorrect username" });
-            }
-            if (!user.checkPassword(password)) {
-                return done(null, false, { message: "Incorrect password"});
-            }
-            return done(null, user);
-        });
-    }
-));
+// passport.use(new LocalStrategy(
+//     {
+//         usernameField: "username"
+//     },
+//     function (username, password, done) {
+//         db.User.findOne({ username }, (err, user) => {
+//             if (err) {
+//                 return done(err);
+//             }
+//             if (!user) {
+//                 return done(null, false, { message: "Incorrect username" });
+//             }
+//             if (!user.checkPassword(password)) {
+//                 return done(null, false, { message: "Incorrect password"});
+//             }
+//             return done(null, user);
+//         });
+//     }
+// ));
 
-passport.serializeUser(db.User.serializeUser());
-passport.deserializeUser(db.User.deserializeUser());
+// passport.serializeUser(db.User.serializeUser());
+// passport.deserializeUser(db.User.deserializeUser());
 
 // Exporting our configured passport
 module.exports = passport;
