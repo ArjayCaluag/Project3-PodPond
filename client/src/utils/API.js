4d969c46275edb70cb50312655ded4ea7f1d931a
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-03-08 11:33:46 -0800

    Create additional methods for API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,5 +8,22 @@ export default {
 
     loginUser: function (userData) {
         return axios.post("/api/login", userData);
+    },
+    
+    getPodcasts: function(){
+        return axios.get("/api/podcasts")
+    },
+
+    savePodcast: function (podcastData){
+        return axios.post ("/api/podcasts", podcastData)
+    },
+
+    deletePodcast: function (id){
+        return axios.delete("/api/podcasts" + id)
+    },
+
+    queryPodcast: function (query){
+        console.log(query);
+        return axios.get("api/podcasts/search", {params: {q:query}});
     }
 }
