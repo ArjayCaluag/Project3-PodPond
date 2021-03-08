e8df13f74c8e814ba7f6a3a1664459f46e5e4bf8
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-03-08 11:04:42 -0800

    Define additional methods for the pondController

--- controller/pondController.js
+++ controller/pondController.js
@@ -8,5 +8,27 @@ module.exports = {
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  // Find podcast with search query
+  findAllPodCast: function (req,res) {
+    db.Podcast
+      .find(req.query)
+      .then(dbModel => res.json (dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // Find podcast by Id
+  findById: function (req,res){
+    db.Podcast
+    .findById(req.params.id)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  // Remove from favorites
+  remove : function (req,res){
+    db.Podcast
+    .findById({ _id: req.params.id})
+    .then(dbModel => dbModel.remove())
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status (422).json(err));
   }
 };
