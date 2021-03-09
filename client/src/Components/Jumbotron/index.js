3b0ba87f40732195190a6c5a0e20c45a6a0b1790
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-03-08 23:27:58 -0800

    Add background image to search jumbotron

--- client/src/Components/Jumbotron/index.js
+++ client/src/Components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Jumbotron() {
   return (
-    <div className="jumbotron jumbotron-fluid">
+    <div className="search jumbotron jumbotron-fluid">
       <div className="container">
         <h1 className="display-4 mt-5">PodPond</h1>
         <p className="lead">
