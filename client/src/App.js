107c9b0860e221ee3b8989bf1008a27610835490
Rand Hale <randluca@gmail.com>
2021-03-10 20:55:07 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar";
